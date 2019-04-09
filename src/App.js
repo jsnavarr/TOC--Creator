3de44565f08db35db3b25023d2319bb2d0e7f011
j salvador navarro <salvador.navarro@gmail.com>
2019-04-09 16:26:32 -0500

    created functions to parse the h tags

--- src/App.js
+++ src/App.js
@@ -4,45 +4,74 @@ import './App.css';
 
 
 class App extends Component {
-  handleCreateTOC(){
-    var input = document.getElementById("HTMLInput").value;
-    var regex1 = /<\s*h[1-5]\s*/g;
-    var regex2 = /<\s*\/h[1-5]\s*>/g;
-    var output=document.getElementById("HTMLOutput");
-    var substring;
-    var idx1=[], idx2=[];
-    var substring1=[];
-    var substring2=[];
-    while ((substring = regex1.exec(input)) !== null) {
-      substring1.push(substring[0]);
-      idx1.push(regex1.lastIndex);
-    }
-    while ((substring = regex2.exec(input)) !== null) {
-      substring2.push(substring[0]);
-      idx2.push(regex2.lastIndex);
-    }
-    console.log(substring1);
-    console.log(idx1);
-    console.log(substring2);
-    console.log(idx2);
 
-    //create the TOC from the index in idx1 and idx2
+  constructor(props) {
+    super(props);
+    this.state = {
+      regex1: /<\s*h[1-5]\s*/g, //regExp for open H tags
+      regex2: /<\s*\/h[1-5]\s*>/g, //regExp for closing H tags
+      output:"" //HTML with TOC
+    };
+  }
+
+  parseHTag(regExp, str){
+    var substring, strResult=[], idxResult=[];
+    while ((substring = regExp.exec(str)) !== null) {
+      strResult.push(substring[0]);
+      idxResult.push(regExp.lastIndex);
+    }
+    return [strResult, idxResult];
+  };
+
+  createTOC(input, HOArray, idxHOA, idxHCA){
     var TOC="";
-    idx1.forEach(function(i, index2){
-      TOC+=substring1[index2]+input.slice(i, idx2[index2])+"\n";
-    })
-    console.log(TOC);
-    output.value=TOC;
+    idxHOA.forEach(function(i, index2){
+      TOC+=HOArray[index2]+input.slice(i, idxHCA[index2])+"\n";
+    });
+    return TOC;
+  }
+
+  handleTextAreaChange = async (event)=>{
+    event.persist();
+    var input = event.currentTarget.value;
+
+    var r = this.parseHTag(this.state.regex1, input);
+    var HOArray = r[0];
+    var idxHOA = r[1];
+
+    console.log('HOArray ', HOArray);
+    console.log('idxHOA ', idxHOA);
+
+    r = this.parseHTag(this.state.regex2, input);
+    var HCArray = r[0];
+    var idxHCA = r[1];
+
+    console.log('HCArray ', HCArray);
+    console.log('idxHCA ', idxHCA);
+
+    // create the TOC from the index in idx1 and idx2
+    var TOC = this.createTOC(input, HOArray, idxHOA, idxHCA);
+
+    //if there is not id in each <H> tag then add one
+
+
+    this.setState(() => ({
+      output: TOC
+    }),()=>{
+      console.log('HTML output \n', this.state.output);
+    });
   }
 
   render() {
     return (
       <div className="App">
         <Segment inverted style={{display: "flex"}}>
-          <textarea id="HTMLInput" rows="40" cols="80"></textarea>
+          <textarea id="HTMLInput" rows="40" cols="80"
+          onChange={this.handleTextAreaChange}
+          ></textarea>
           <div>
             <Button positive
-              onClick={this.handleCreateTOC}
+              // onClick={this.handleCreateTOC}
             >Create TOC</Button>
           </div>
           <textarea id="HTMLOutput" rows="40" cols="80"></textarea>
