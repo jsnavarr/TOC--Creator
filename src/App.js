463b4698cfb211c5985f31e5e1a888a80507e355
j salvador navarro <salvador.navarro@gmail.com>
2019-04-10 21:10:39 -0500

    changed the way H tags are parsed and created TOC with ul and li tags

--- src/App.js
+++ src/App.js
@@ -8,58 +8,147 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      regex1: /<\s*h[1-5]\s*/g, //regExp for open H tags
+      regex1: /(<\s*h([1-5]))\s*(id\s*=\s*"(\w+)"\s*)*>/g, //regExp for open H tags with and without id
       regex2: /<\s*\/h[1-5]\s*>/g, //regExp for closing H tags
       output:"" //HTML with TOC
     };
   }
 
-  parseHTag(regExp, str){
-    var substring, strResult=[], idxResult=[];
-    while ((substring = regExp.exec(str)) !== null) {
-      strResult.push(substring[0]);
-      idxResult.push(regExp.lastIndex);
+  formTOC (TOC, HNumberArray){
+    //add the first TOC line
+    var newTOC="<ul>\n\t<li>"+TOC[0]+"</li>\n";
+    var tabs="\t";
+    //loop through each element (starting in the 2nd)
+    for(var i=1; i<TOC.length; i++){
+      if(HNumberArray[i]===HNumberArray[i-1]){
+        //if H tag number is the same than previous one then just add <li> tags
+        newTOC+=tabs+"<li>"+TOC[i]+"</li>\n";
+      } else if(HNumberArray[i]>HNumberArray[i-1]){
+        //if H tag number is bigger than previous one then add <ul> and <li>
+        newTOC+=tabs+"<ul>\n";
+        //<li> will have one level more of indentation
+        tabs+="\t";
+        newTOC+=tabs+"<li>"+TOC[i]+"</li>\n";
+      } else {
+        //H tag number is smaller than previous one then close <ul>
+        //with one level less of indentation
+        tabs=tabs.slice(1, tabs.length);
+        newTOC+=tabs+"</ul>\n";
+        //add the h tab inside an <li> tag
+        newTOC+=tabs+"<li>"+TOC[i]+"</li>\n";
+      }
+      console.log("tabs ", tabs.length);
     }
-    return [strResult, idxResult];
-  };
-
-  createTOC(input, HOArray, idxHOA, idxHCA){
-    var TOC="";
-    idxHOA.forEach(function(i, index2){
-      TOC+=HOArray[index2]+input.slice(i, idxHCA[index2])+"\n";
-    });
-    return TOC;
+    //close any remaining <ul>
+    var regExp=/\t/;
+    //make a copy of the tabs
+    var newTabs = tabs;
+    //one closing <ul> for any remaining tab
+/*    while(regExp.exec(tabs)!=null){
+      newTabs.slice(1, newTabs.length);
+      newTOC+=newTabs+"</ul>";
+    }*/
+    //need to close always one <ul> because the first one has no indentation
+    newTOC+="</ul>";
+    console.log('Final TOC \n', newTOC);
+    return newTOC;
   }
+
+  parseHTag(regExpO, regExpC, str){
+    var regExpOIdx=0; //index returned by parsing the opening H tag
+    var regExpCIdx=0; //index returned by parsing the closing H tag
+    var inputIdx=0; //index to the start of next frame from str (input) to be concatenated to the output
+    var TOCline=""; //HTML line created which will be added to the TOC
+    var TOC=[]; //The TOC
+    var openingH; //to store the result from executing the regExp to find the opening H tag
+    var closingH; //to store the result from executing the regExp to find the closing H tag
+    var HNumberArray=[]; //to remember each H number (1..5) for each entry in the TOC
+    var id=12345678; //temporary number to use for the newly added IDs
+    var output=""; //the HTML output with TOC on the top
+    var updatedHTag; //to store the updated H tag (with a newly added ID when none is present)
+    const regExp=/<\s*h[1-5]\s*id\s*=\s*"\w*"\s*>/; // regular expression to test if tag has already an ID
+    const regExpCH=/<\s*\/h[1-5]\s*>/;
+    //parse the HTML input file while there are opening H tags
+    while((openingH = regExpO.exec(str))!== null){
+      //get the index of the opening H tag
+      regExpOIdx = regExpO.lastIndex; 
+      //get the closing H tag
+      closingH = regExpC.exec(str);
+      //get the index of the closing H tag
+      regExpCIdx = regExpC.lastIndex;
+
+      if(openingH && closingH){
+        //check if there is already an "id"
+        if(!regExp.test(openingH)){
+          console.log('there is no ID');
+          //increase id so it is unique
+          id+=1;
+          //create the new entry to be added to the TOC
+          //it will start with the opening H, add an anchor tag referencing the new id
+          //then add the rest of the content from where the opening H tag ends to the end
+          //of the closing H tag
+          //close the anchor tag
+          TOCline=`<a href="#${id}">`+(str.slice(regExpOIdx, regExpCIdx)).replace(regExpCH, '')+'</a>';
+          //create the content to be added to the output 
+          //starting from the end of previous closing H tag to the start of this opening H tag
+          //then add the opening H tag followed by the new id
+          //then add whatever is inside the H tags and the closing H tag
+          updatedHTag=str.slice(inputIdx, regExpOIdx-openingH.length-1)+openingH[1]+` id="${id}">`+str.slice(regExpOIdx,regExpCIdx+1);
+          //add the updated H tag to the output
+          output+=updatedHTag;
+          //add the new TOC line to TOC
+          // TOC+=TOCline+"\n";
+          //update the pointer to be used by the next entry, it is the index of the closing H tag
+          inputIdx= regExpCIdx+1;
+        } else {
+          //there is already an ID
+          console.log('existing ID ', openingH[2]);
+          //create the new entry to be added to the TOC
+          //it will start with the opening H. Add an anchor tag referencing the existing id
+          //then add the rest of the content from where the opening H tag ends to the end
+          //of the closing H tag
+          //close the anchor tag
+          TOCline=`<a href="#${openingH[4]}"`+'>'+(str.slice(regExpOIdx, regExpCIdx)).replace(regExpCH, '')+'</a>';
+          //create the content to be added to the output 
+          //starting from the end of previous closing H tag
+          //including this H tag as is (since it has already an id) 
+          //then add whatever is inside the H tags and the closing H tag
+          updatedHTag=str.slice(inputIdx, regExpCIdx+1);
+          //add the updated H tag to the output
+          output+=updatedHTag;
+          //add the new TOC line to TOC
+          // TOC+=TOCline+"\n";
+          //update the pointer to be used by the next entry, it is the index of the closing H tag
+          inputIdx= regExpCIdx+1;
+        } 
+        //add the TOCline to the TOC 
+        TOC.push(TOCline);
+        //kepp track of the H tag numbers (1..5)
+        // console.log('H number ', openingH[1], openingH[2], openingH[3])
+        HNumberArray.push(parseInt(openingH[2]));
+        //  = this.addLineToTOC(TOCline, TOC, prevHNumber, parseInt(openingH[2]));
+        // prevHNumber=parseInt(openingH[2]);
+      } else {
+        console.log('Some of the opening/closing H tags did not match');
+        return null;
+      }
+    }
+    //get the rest of the input from regExpCIdx to the end of the HTML input file
+    output+=str.slice(regExpCIdx,str.length);
+    //form the TOC with proper ul and li tags
+    TOC = this.formTOC(TOC, HNumberArray);
+    // console.log('new input \n'+str);
+    console.log('TOC \n'+TOC);
+    console.log('H tags \n'+HNumberArray);
+    console.log('output --\n', output);
+    return [output, TOC];
+  };
 
   handleTextAreaChange = async (event)=>{
     event.persist();
     var input = event.currentTarget.value;
 
-    var r = this.parseHTag(this.state.regex1, input);
-    var HOArray = r[0];
-    var idxHOA = r[1];
-
-    console.log('HOArray ', HOArray);
-    console.log('idxHOA ', idxHOA);
-
-    r = this.parseHTag(this.state.regex2, input);
-    var HCArray = r[0];
-    var idxHCA = r[1];
-
-    console.log('HCArray ', HCArray);
-    console.log('idxHCA ', idxHCA);
-
-    // create the TOC from the index in idx1 and idx2
-    var TOC = this.createTOC(input, HOArray, idxHOA, idxHCA);
-
-    //if there is not id in each <H> tag then add one
-
-
-    this.setState(() => ({
-      output: TOC
-    }),()=>{
-      console.log('HTML output \n', this.state.output);
-    });
+    var r = this.parseHTag(this.state.regex1, this.state.regex2, input);
   }
 
   render() {
